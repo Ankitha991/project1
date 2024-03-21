c1f3d06e83d395b99e187fc15f09b56c8680eab2
Ankitha991 <ankithareddy691@gmail.com>
2024-03-21 10:09:55 +0530

    This is my first commit.

---
+++ Even.java
@@ -0,0 +1,17 @@
+
+public class Even
+{
+	static boolean checkForEven(int n)
+	{
+		if(n%2==0)
+		{
+			//System.out.println(n+" is even number");
+			return true;
+		}
+		else
+		{
+			//System.out.println(n+" is odd number");
+			return false;
+		}
+	}
+}
---
+++ IfElse.java
@@ -0,0 +1,20 @@
+package sampleproject.java;
+
+public class IfElse 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=6;
+		if(i > 5)
+		{
+	      System.out.println("Number is greater");
+		}
+		else
+		{
+			System.out.println("Number is lesser than other number");
+		}
+	
+	}
+
+}
